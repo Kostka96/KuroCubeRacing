d35f850fb4323f94fbeeb614f9db9afbec055620
Kostka96 <reutsa51@gmail.com>
2026-05-14 16:16:52 +0300

    + wifi

--- TeleTracker.ino
+++ TeleTracker.ino
@@ -1,83 +1,119 @@
+#include <WiFi.h>
+#include <WiFiUdp.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 #include <TFT_eSPI.h>
+#include "config.h"
+
+WiFiUDP udp;
+char incomingPacket[255];
 
 TFT_eSPI tft = TFT_eSPI();
 TFT_eSprite canvas = TFT_eSprite(&tft);
 Adafruit_MPU6050 mpu;
 
-#define CORRECTION_ANGLE 88 
-// Уменьшаем до 180. Это даст огромный прирост FPS.
-#define S_SIZE 180 
-#define S_CENTER (S_SIZE / 2)
-
-int rpm = 0;
-unsigned long lastTime = 0;
+// Общие переменные
+volatile int sharedRpm = 0;
+float sharedAngle = 0; 
 float fps = 0;
 
 void setup() {
   Serial.begin(115200);
-  
-  Wire.begin(21, 22);
-  Wire.setClock(400000);
 
+  // 1. Сначала графика (резервируем память)
   tft.init();
   tft.setRotation(0);
   tft.fillScreen(TFT_BLACK);
-  // Точка поворота на самом экране ВСЕГДА 120, 120 (центр дисплея)
-  tft.setPivot(120, 120); 
-  
-  canvas.setColorDepth(16); 
-  if (!canvas.createSprite(S_SIZE, S_SIZE)) {
-    while(1);
-  }
-  // Точка поворота внутри спрайта - его собственный центр
+  tft.setPivot(120, 120);
+  canvas.setColorDepth(16); // Раз 16 бит летает — оставляем их!
+  canvas.createSprite(S_SIZE, S_SIZE);
   canvas.setPivot(S_CENTER, S_CENTER);
 
-  if (!mpu.begin()) { Serial.println("No MPU"); }
+  // 2. Инициализация I2C для MPU
+  Wire.begin(21, 22);
+  Wire.setClock(400000);
+  Wire.setTimeOut(10); // Защита от зависания шины
+
+  mpu.begin();
   mpu.setFilterBandwidth(MPU6050_BAND_94_HZ);
+
+  // 3. Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  udp.begin(localUdpPort);
+
+  // 4. Запуск задачи на Ядре 0
+  xTaskCreatePinnedToCore(
+    coreTaskZero,   // Функция
+    "TaskZero",     // Название
+    8192,           // Стек
+    NULL,           // Параметры
+    1,              // Приоритет
+    NULL,           // Хендл
+    0               // Ядро 0
+  );
 }
 
-void loop() {
-  // Считаем FPS
-  unsigned long currentTime = millis();
-  float currentFrameTime = (currentTime - lastTime);
-  lastTime = currentTime;
-  if (currentFrameTime > 0) fps = (fps * 0.9) + (1000.0 / currentFrameTime * 0.1);
-
-  // Читаем датчик
+// --- ЗАДАЧА НА ЯДРЕ 0: ДАТЧИК + ГРАФИКА ---
+void coreTaskZero(void * pvParameters) {
+  unsigned long lastTime = 0;
   sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-  
-  // Быстрая математика
-  float rawAngle = atan2(a.acceleration.x, a.acceleration.y) * 57.29578; 
-  float displayAngle = rawAngle + CORRECTION_ANGLE;
-  
-  canvas.fillSprite(TFT_BLACK);
-  
-  // Рисуем дуги чуть меньшего радиуса, чтобы влезли в 180х180
-  // drawSmoothArc(x, y, R_out, R_in, start, end, color, back_color)
-  canvas.drawSmoothArc(S_CENTER, S_CENTER, 88, 84, 220, 140, TFT_DARKGREY, TFT_BLACK);
-  
-  int rpmAngle = map(rpm, 0, 8000, 220, 500);
-  canvas.drawSmoothArc(S_CENTER, S_CENTER, 88, 80, 220, rpmAngle, TFT_WHITE, TFT_BLACK);
-  
-  // Линия
-  canvas.drawFastHLine(S_CENTER - 50, S_CENTER + 20, 100, TFT_WHITE);
-  
-  // Данные (используем drawNumber для скорости)
-  canvas.setTextColor(TFT_WHITE);
-  canvas.drawNumber(rpm, S_CENTER - 25, S_CENTER - 10, 4);
-  
-  // Вывод FPS
-  canvas.setTextColor(TFT_WHITE);
-  canvas.drawNumber((int)fps, S_CENTER - 10, 10, 2);
-  
-  // ПОВОРОТ И ОТПРАВКА
-  // Теперь процессор крутит на 25% меньше пикселей!
-  canvas.pushRotated(displayAngle);
-  
-  rpm += 150;
-  if (rpm > 8000) rpm = 0;
+
+  for(;;) {
+    // А) Опрос MPU6050
+    if (mpu.getEvent(&a, &g, &temp)) {
+      float rawAngle = atan2(a.acceleration.x, a.acceleration.y) * 57.29578;
+      sharedAngle = rawAngle + CORRECTION_ANGLE;
+    }
+
+    // Б) Отрисовка
+    unsigned long currentTime = millis();
+    if (currentTime - lastTime > 0) 
+      fps = (fps * 0.9) + (1000.0 / (currentTime - lastTime) * 0.1);
+    lastTime = currentTime;
+
+    canvas.fillSprite(TFT_BLACK);
+    
+    // Рисуем дуги
+    canvas.drawSmoothArc(S_CENTER, S_CENTER, 88, 84, 220, 140, TFT_DARKGREY, TFT_BLACK);
+    int r = sharedRpm;
+    int rpmAngle = map(r, 0, 8000, 220, 500);
+    canvas.drawSmoothArc(S_CENTER, S_CENTER, 88, 80, 220, rpmAngle, TFT_WHITE, TFT_BLACK);
+    
+    // Линия горизонта
+    canvas.drawFastHLine(S_CENTER - 50, S_CENTER + 20, 100, TFT_WHITE);
+    
+    // Текст
+    canvas.setTextColor(TFT_WHITE);
+    canvas.drawNumber(r, S_CENTER - 25, S_CENTER - 10, 4);
+    canvas.drawNumber((int)fps, S_CENTER - 10, 10, 2);
+    
+    // Вывод на экран
+    canvas.pushRotated(sharedAngle);
+    
+    // Важнейшая задержка для стабильности
+    vTaskDelay(2 / portTICK_PERIOD_MS); 
+  }
+}
+
+// --- ЯДРО 1: ТОЛЬКО СЕТЬ ---
+void loop() {
+  // 1. Обработка UDP
+  int packetSize = udp.parsePacket();
+  if (packetSize) {
+    int len = udp.read(incomingPacket, 255);
+    if (len > 0) incomingPacket[len] = 0;
+    // Предполагаем, что пришло просто число "6500"
+    sharedRpm = atoi(incomingPacket); 
+  }
+  // Даем время фоновым процессам Wi-Fi
+  yield();
+  delay(5); 
 }
